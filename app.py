cfa002ba56f3ffe4b335553cf8bead4026cc5bbc
Praful Dhakal <praful.dhakal11@gmail.com>
2026-08-23 15:36:38 +0545

    Number of messages in the group (overall and single user)

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import preprocessor
+import preprocessor, helper
 
 st.sidebar.title("Whatsapp Chat Analyzer")
 
@@ -19,11 +19,13 @@ if uploaded_file is not None:
     user_list.sort()
     user_list.insert(0, "Overall")
  
-    st.sidebar.selectbox("Show analysis with respect to", user_list)
+    selected_user = st.sidebar.selectbox("Show analysis with respect to", user_list)
 
     if st.sidebar.button("Show Analysis"):
 
+        num_messages = helper.fetch_stats(selected_user, df)
         col1, col2, col3, col4 = st.columns(4)
 
         with col1: 
             st.header("Total Messages")
+            st.title(num_messages)
