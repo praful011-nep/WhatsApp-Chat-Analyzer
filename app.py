a14bb73ffdef8f533de04f0e7aaddc89b08ae4b4
Praful Dhakal <praful.dhakal11@gmail.com>
2026-08-23 15:21:37 +0545

    Remove group_notification; Show Analysis sidebar

--- app.py
+++ app.py
@@ -14,4 +14,16 @@ if uploaded_file is not None:
     #Fetching unique users
     user_list = df['user'].unique().tolist()
 
+    #Remove group_notification and add overall in users
+    user_list.remove('group_notification')
+    user_list.sort()
+    user_list.insert(0, "Overall")
+ 
     st.sidebar.selectbox("Show analysis with respect to", user_list)
+
+    if st.sidebar.button("Show Analysis"):
+
+        col1, col2, col3, col4 = st.columns(4)
+
+        with col1: 
+            st.header("Total Messages")
