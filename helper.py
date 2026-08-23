cb454c1fb3c438cf31e05a810969aa6997f3d376
Praful Dhakal <praful.dhakal11@gmail.com>
2026-08-23 15:57:14 +0545

    Restructured helper.py

--- helper.py
+++ helper.py
@@ -1,20 +1,17 @@
 def fetch_stats(selected_user, df):
 
-    if selected_user == "Overall":
-        #Number of messages
-        num_message  = df.shape[0]
+    if selected_user != "Overall":
+        df = df[df["user"] == selected_user]
 
-        #Number of words
-        words = []
-        for message in df['message']:
-            words.extend(message.split())
-        return num_message, len(words)
-    else:
-        #Number of message
-        new_df = df[df['user'] == selected_user]
-        num_message = new_df.shape[0]
-        #Number of words
-        words = []
-        for message in new_df['message']:
-            words.extend(message.split())
-        return num_message, len(words)
+    # Number of Messages
+    num_message = df.shape[0]
+   
+    # Number of Words
+
+    words = []
+    for message in df['message']:
+        words.extend(message.split())
+
+    return num_message, len(words)
+        
+
